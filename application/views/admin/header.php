dfa63a682fa4394d5cdf793a66c15026d488cebd
kingola1 <segun8427@gmail.com>
2025-10-22 21:26:37 +0100

    Added Project for finance

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -295,8 +295,20 @@
               </ul>
             </li>
 
-
-
+            <!-- Projects Management -->
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-tasks"></i> <span>Projects</span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="<?php echo site_url('projects'); ?>"><i class="fa fa-circle-o"></i> All Projects</a></li>
+                <li><a href="<?php echo site_url('projects/create'); ?>"><i class="fa fa-plus-circle"></i> Create Project</a></li>
+              </ul>
+            </li>
+            
             <!-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-ticket"></i> <span>Voucher</span>
